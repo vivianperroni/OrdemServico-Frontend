498675d7949e4e8d421da6fb19a043cae6a57303
vivianperroni <vivian.perroni@hotmail.com>
2021-12-08 19:51:59 -0400

    Implantação frontend

---
+++ src/app/apiservice.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+class Apiservice {
+
+    constructor(apiurl){
+        this.apiurl = apiurl;
+    }
+
+    post(url, objeto){
+        return httpClient.post(this.requestUrl(url), objeto);
+    }
+    put(url, objeto){
+        return httpClient.put(this.requestUrl(url), objeto);
+    }
+    delete(url){
+        return httpClient.delete(this.requestUrl(url));
+    }
+    get(url){
+        return httpClient.get(this.requestUrl(url));
+    }
+    requestUrl(url){
+        return `${this.apiurl}${url}`;
+    }
+}
+export default Apiservice;
